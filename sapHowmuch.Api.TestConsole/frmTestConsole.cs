cd437301463b6e7fb79e4169775efb7b4705dcb5
Jun Yu <shockz@iquest.co.kr>
2017-03-31 11:46:40 +0900

    체리 픽

---
+++ sapHowmuch.Api.TestConsole/frmTestConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sapHowmuch.Api.TestConsole
+{
+	public partial class frmTestConsole : Form
+	{
+		private const string _apiBaseAddress = "http://192.168.1.229:9091/";
+
+		public frmTestConsole()
+		{
+			InitializeComponent();
+		}
+	}
+}
